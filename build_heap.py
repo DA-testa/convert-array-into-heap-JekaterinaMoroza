23dcd7ec4aa1e657d185e4c21ad70d1b2133b16f
JekaterinaMoroza <125254572+JekaterinaMoroza@users.noreply.github.com>
2023-03-19 16:20:03 +0000

    Pirmais Commit

--- build_heap.py
+++ build_heap.py
@@ -2,24 +2,45 @@
 
 
 def build_heap(data):
+    n = len(data)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    def sift_down(i):
+        min_index= i
+        right_child= 2*i+2
+        left_child= 2*i+1
+        
+        if left_child < len(data) and data[left_child] < data [min_index]:
+            min_index= left_child
 
+        if right_child < len(data) and data[right_child] < data [min_index]:
+            min_index= right_child
+
+        if i != min_index:
+            data[i], data[min_index] = data[min_index], data[i]
+            swaps.append((i, min_index))
+            sift_down(min_index)
+
+
+    for i in range (n//2, -1, -1):
+        sift_down(i)
 
     return swaps
 
 
 def main():
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    inputs = input("Izvēlies I vai F: ")
+    if "I" in inputs:
+        n = int(input("Ievadi daudzumu: "))
+        data = list(map(int, input("Ievadi skaitļus: ").split()))
 
+    # checks if lenght of data is the same as the said lenght
+    elif "F" in inputs:
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+        filename = input("Faila nosaukums: ")
+        with open("tests/"+filename, 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
